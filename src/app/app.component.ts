fe95b225804086f2a6557f829017de08e3579e7b
Neeraj Negi <neeraj.negi174@gmail.com>
2019-01-10 16:40:13 +0530

    Made whole page linearly scrollable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
+  currentRoute: string = 'summary';
 
   constructor() {}
 
   ngOnInit() {
 
+  }
+
+  scroll($el, route:string) {
+    console.log($el);
+    $el.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+    this.currentRoute = route;
   }
     
 }
